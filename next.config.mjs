f999d422d2ca8738b07274dfb1a9cab23465f21b
Jakub Filistyński <j.filistynski@gmail.com>
2024-04-02 13:29:02 +0200

    Configure next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/coffee-brew-timer",
+  output: "export",  // <=== enables static exports
+};
 
 export default nextConfig;
