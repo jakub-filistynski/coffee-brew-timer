017cb76b02a8f693ad237555c35de99a96101877
maclewan <maciek.lewandowicz@gmail.com>
2024-04-10 15:06:18 +0200

    feat: refactor HoffmanBetterOneCupMethodSteps

--- src/app/lib/recipies/drip/hoffman.ts
+++ src/app/lib/recipies/drip/hoffman.ts
@@ -1,22 +1,28 @@
 import { Step } from "@/app/lib/definitions";
 
-export const getHoffmanMethoSteps = (waterAmount: number): Array<Step> => {
+export const getHoffmanBetterOneCupMethodSteps = (totalWaterAmount: number): Array<Step> => {
+  let stepWaterAmount: number = Math.round(totalWaterAmount / 5)
   return [
-    { message: `Add ${waterAmount} g of water in 10 seconds`, deadline: 10 }, // 10
-    { message: "Swirl the drip for 5 seconds", deadline: 15 }, // 15
-    { message: "Wait for 30 seconds", deadline: 45 }, // 45
-    { message: `Add ${waterAmount} g of water in 15 seconds`, deadline: 60 }, // 1:00
-    { message: "Wait for 10 seconds", deadline: 70 }, // 1:10
-    { message: `Add ${waterAmount} g of water in 10 seconds`, deadline: 80 }, // 1:20
-    { message: "Wait for 10 seconds", deadline: 90 }, // 1:30
-    { message: `Add ${waterAmount} g of water in 10 seconds`, deadline: 100 }, // 1:40
-    { message: "Wait for 10 seconds", deadline: 110 }, // 1:50
-    { message: `Add ${waterAmount} g of water in 10 seconds`, deadline: 120 }, // 2:00
-    { message: "Gentle swirl the drip for 5 seconds", deadline: 125 }, // 2:05
+    // pre-infusion 0:00-0:45
+    { deadline: 10, message: `Add ${stepWaterAmount} g of water in 10 seconds` },
+    { deadline: 15, message: "Swirl the drip for 5 seconds" },
+    { deadline: 45, message: "Wait for 30 seconds" },
+    // 1st batch 0:45-1:10
+    { deadline: 55, message: `Add ${stepWaterAmount} g of water in 10 seconds` },
+    { deadline: 70, message: "Wait for 15 seconds" },
+    // 2nd batch 1:10-1:30
+    { deadline: 80, message: `Add ${stepWaterAmount} g of water in 10 seconds` },
+    { deadline: 90, message: "Wait for 10 seconds" },
+    // 3nd batch 1:30-1:50
+    { deadline: 100, message: `Add ${stepWaterAmount} g of water in 10 seconds` },
+    { deadline: 110, message: "Wait for 10 seconds" },
+    // final batch 1:50-end
+    { deadline: 120, message: `Add ${stepWaterAmount} g of water in 10 seconds` },
+    { deadline: 125, message: "Gentle swirl the drip for 5 seconds" },
     {
+      deadline: 300,
       message:
-        "Wait for the water to flow (Ideally, the water should drain to +- 3:30 minutes)",
-      deadline: 210,
+        "Wait for the water to flow (Ideally, the water should drain to +- 3:00 minutes)",
     }, // The last step without deadline
   ];
 };
