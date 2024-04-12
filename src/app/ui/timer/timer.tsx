4f404500c9c42253033d49980cb3e21974072da6
maclewan <maciek.lewandowicz@gmail.com>
2024-04-12 12:07:42 +0200

    feat(timer + recipe schema): add stage and comment to schema

--- src/app/ui/timer/timer.tsx
+++ src/app/ui/timer/timer.tsx
@@ -54,16 +54,28 @@ export function Timer({ steps }: Props) {
   return (
     <div className="flex items-center justify-center h-screen">
       <div className="text-center space-y-5 flex flex-col">
-        <span className="text-2xlsm:text-4xl ">
+        <div className="text-4xl sm:text-6xl h-10 sm:h-16">
+          {steps[currentStepIndex].stage ?
+            `Stage: ${steps[currentStepIndex].stage}` : null
+          }
+        </div>
+        <div className="text-4xl sm:text-8xl  flex flex-col space-y-5">
+          <span>{steps[currentStepIndex].message}</span>
+        </div>
+        <div className="text-2xl sm:text-4xl h-8 sm:h-10">
+          {steps[currentStepIndex].comment ?
+            steps[currentStepIndex].comment: null
+          }
+        </div>
+        <div className="text-4xl sm:text-8xl  flex flex-col space-y-5">
+          <span>Left: {steps[currentStepIndex].deadline - totalSeconds} s</span>
+        </div>
+        <span className="text-2xl sm:text-4xl ">
           <b>Next step: </b>{" "}
           {currentStepIndex === steps.length
             ? ""
             : steps[currentStepIndex + 1].message}
         </span>
-        <div className="text-4xl sm:text-8xl  flex flex-col space-y-5">
-          <span>{steps[currentStepIndex].message}</span>
-          <span>Left: {steps[currentStepIndex].deadline - totalSeconds} s</span>
-        </div>
         <span className="sm:text-2xl text-xl">Total: {totalSeconds}</span>
         <div>
           <span className="inline-flex -space-x-px overflow-hidden rounded-md border bg-white shadow-sm">
