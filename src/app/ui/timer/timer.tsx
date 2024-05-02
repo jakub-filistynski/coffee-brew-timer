2b2012650ae5d0b2f286d0034b282b4c8a246256
Jakub Filistyński <83501516+jakub-filistynski@users.noreply.github.com>
2024-05-02 18:54:30 +0200

    13 fix last step bug (#14)
    * Change application name in a browser tab
    
    * Fix not changing step issue
    
    ---------
    
    Co-authored-by: Jakub Filistyński <jakub.filistynski@gmail.com>

--- src/app/ui/timer/timer.tsx
+++ src/app/ui/timer/timer.tsx
@@ -1,40 +1,30 @@
 "use client";
 
-import {BrewingRecipe, BrewingStep} from "@/app/lib/definitions";
+import { BrewingRecipe, BrewingStep } from "@/app/lib/definitions";
 import { PauseResumeButton } from "@/app/ui/timer/pauseResumeButton";
 import { useStopwatch } from "react-timer-hook";
 import { useState, useEffect } from "react";
-import {addLeadingZeros} from "@/app/ui/utils";
-
+import { addLeadingZeros } from "@/app/ui/utils";
 
 type Props = {
   brewingRecipe: BrewingRecipe | undefined;
 };
 
-
-export function Timer({ brewingRecipe } : Props) {
-  const {
-    totalSeconds,
-    seconds,
-    minutes,
-    isRunning,
-    start,
-    pause,
-    reset,
-  } = useStopwatch({ autoStart: false });
-  let steps: BrewingStep[] = []
+export function Timer({ brewingRecipe }: Props) {
+  const { totalSeconds, seconds, minutes, isRunning, start, pause, reset } =
+    useStopwatch({ autoStart: false });
+  let steps: BrewingStep[] = [];
   if (brewingRecipe === undefined) {
-    steps = [{message: "-", deadline: ""}] as BrewingStep[]
-  }
-  else {
-    steps = brewingRecipe.brewingSteps
+    steps = [{ message: "-", deadline: "" }] as BrewingStep[];
+  } else {
+    steps = brewingRecipe.brewingSteps;
   }
 
   const [currentStepIndex, setCurrentStepIndex] = useState(0);
 
   useEffect(() => {
-        handleReset()
-    },[brewingRecipe])
+    handleReset();
+  }, [brewingRecipe]);
 
   useEffect(() => {
     const currentStep: BrewingStep = steps[currentStepIndex];
@@ -42,7 +32,7 @@ export function Timer({ brewingRecipe } : Props) {
       return;
     }
 
-    if (totalSeconds === currentStep.deadline) {
+    if (totalSeconds >= currentStep.deadline) {
       setCurrentStepIndex(currentStepIndex + 1);
     }
   }, [totalSeconds]);
@@ -66,54 +56,55 @@ export function Timer({ brewingRecipe } : Props) {
 
   return (
     <>
-        <div className="text-4xl sm:text-6xl h-10 sm:h-16">
-          {steps[currentStepIndex].stage ?
-            `Stage: ${steps[currentStepIndex].stage}` : null
-          }
-        </div>
-        <div className="text-4xl sm:text-8xl  flex flex-col space-y-5">
-          <span>{steps[currentStepIndex].message}</span>
-        </div>
-        <div className="text-2xl sm:text-4xl h-8 sm:h-10">
-          {steps[currentStepIndex].comment ?
-            steps[currentStepIndex].comment: null
-          }
-        </div>
-        <div className="text-4xl sm:text-8xl  flex flex-col space-y-5">
-          <span>Left: {steps[currentStepIndex].deadline - totalSeconds} s</span>
-        </div>
-        <span className="text-2xl sm:text-4xl ">
-          <b>Next step: </b>{" "}
-          {currentStepIndex + 1 === steps.length
-            ? ""
-            : steps[currentStepIndex + 1].message}
-        </span>
-        <span className="sm:text-2xl text-xl">Total: {addLeadingZeros(minutes, 2)}:{addLeadingZeros(seconds, 2)}</span>
-        <div>
-          <span className="inline-flex -space-x-px overflow-hidden rounded-md border bg-white shadow-sm">
-            {totalSeconds === 0 ? (
-              <button
-                onClick={start}
-                disabled={brewingRecipe === undefined}
-                className="inline-block px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:relative"
-              >
-                Start
-              </button>
-            ) : (
-              <PauseResumeButton
-                onClick={handlePauseAndResume}
-                isTimerPaused={isPaused()}
-              />
-            )}
+      <div className="text-4xl sm:text-6xl h-10 sm:h-16">
+        {steps[currentStepIndex].stage
+          ? `Stage: ${steps[currentStepIndex].stage}`
+          : null}
+      </div>
+      <div className="text-4xl sm:text-8xl  flex flex-col space-y-5">
+        <span>{steps[currentStepIndex].message}</span>
+      </div>
+      <div className="text-2xl sm:text-4xl h-8 sm:h-10">
+        {steps[currentStepIndex].comment
+          ? steps[currentStepIndex].comment
+          : null}
+      </div>
+      <div className="text-4xl sm:text-8xl  flex flex-col space-y-5">
+        <span>Left: {steps[currentStepIndex].deadline - totalSeconds} s</span>
+      </div>
+      <span className="text-2xl sm:text-4xl ">
+        <b>Next step: </b>{" "}
+        {currentStepIndex + 1 === steps.length
+          ? ""
+          : steps[currentStepIndex + 1].message}
+      </span>
+      <span className="sm:text-2xl text-xl">
+        Total: {addLeadingZeros(minutes, 2)}:{addLeadingZeros(seconds, 2)}
+      </span>
+      <div>
+        <span className="inline-flex -space-x-px overflow-hidden rounded-md border bg-white shadow-sm">
+          {totalSeconds === 0 ? (
             <button
-              onClick={handleReset}
+              onClick={start}
+              disabled={brewingRecipe === undefined}
               className="inline-block px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:relative"
             >
-              Reset
+              Start
             </button>
-          </span>
-        </div>
-      </>
-
+          ) : (
+            <PauseResumeButton
+              onClick={handlePauseAndResume}
+              isTimerPaused={isPaused()}
+            />
+          )}
+          <button
+            onClick={handleReset}
+            className="inline-block px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:relative"
+          >
+            Reset
+          </button>
+        </span>
+      </div>
+    </>
   );
 }
